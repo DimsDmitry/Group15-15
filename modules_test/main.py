c61772fb213396f9d1949d6acc9806e8e8aa7def
Dmitriy <dmitriy250300@mail.ru>
2023-05-12 18:04:49 +0300

    12.05.2023

--- modules_test/main.py
+++ modules_test/main.py
@@ -1,8 +1,18 @@
-from testmodule import *
+from sport_rules import *
 
 
-result1 = multiply(4, 8)
-result2 = plus(2, 4)
+print('Сегодня в программе:\n1-футбол\n2-хоккей\n3-бег на 100 м')
+answer = input('Ваш выбор (off - завершить):')
+while answer != 'off':
+    if answer == '1':
+        print_football()
+    elif answer == '2':
+        print_hockey()
+    elif answer == '3':
+        print_sprint()
+    else:
+        print('Данный вид спорта не найден!')
+    answer = input('Ваш выбор (off - завершить):')
 
-print(result1)
-print(result2)
+
+
