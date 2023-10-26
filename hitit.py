d6224628f6388dae75bbaa823d1ca7b731a602c8
Dmitriy <dmitriy250300@mail.ru>
2023-10-26 12:15:35 +0300

    26.10.2023

--- hitit.py
+++ hitit.py
@@ -1,5 +1,10 @@
 from turtle import *
 
+tsize = 20
+s_width = 200
+s_height = 180
+total_score = 0
+
 
 class Sprite(Turtle):
     # класс-наследник черепашки
@@ -12,18 +17,47 @@ class Sprite(Turtle):
         self.shape(shape)
         self.step = step
         self.points = 0
+
     # движение черепашки:
     def move_up(self):
         self.goto(self.xcor(), self.ycor() + self.step)
+
     def move_down(self):
         self.goto(self.xcor(), self.ycor() - self.step)
+
     def move_left(self):
         self.goto(self.xcor() - self.step, self.ycor())
+
     def move_right(self):
         self.goto(self.xcor() + self.step, self.ycor())
 
+    def is_collide(self, sprite):
+        dist = self.distance(sprite.xcor(), sprite.ycor())
+        if dist < 30:
+            return True
+        return False
+
+    def set_move(self, x_start, y_start, x_end, y_end):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.x_end = x_end
+        self.y_end = y_end
+        self.goto(x_start, y_start)
+        self.setheading(self.towards(x_end, y_end))  # указываем направление черепашки
+
+    def make_step(self):
+        self.forward(self.step)
+        if self.distance(self.x_end, self.y_end) < self.step:
+            self.set_move(self.x_end, self.y_end, self.x_start, self.y_start)
+
 
 player = Sprite(0, -100, 10, 'circle', 'orange')
+enemy1 = Sprite(-s_width, 0, 15, 'square', 'red')
+enemy2 = Sprite(s_width, 0, 15, 'square', 'red')
+goal = Sprite(0, 120, 20, 'triangle', 'green')
+
+enemy1.set_move(-s_width, 0, s_width, 0)
+enemy2.set_move(s_width, 70, -s_width, 70)
 
 scr = player.getscreen()
 scr.listen()
@@ -32,5 +66,16 @@ scr.onkey(player.move_down, 'Down')
 scr.onkey(player.move_left, 'Left')
 scr.onkey(player.move_right, 'Right')
 
+while total_score < 3:
+    enemy1.make_step()
+    enemy2.make_step()
+    if player.is_collide(goal):
+        total_score += 1
+        player.goto(0, -100)
+    if player.is_collide(enemy1) or player.is_collide(enemy2):
+        goal.hideturtle()
+        break
 
-
+    if total_score == 3:
+        enemy1.hideturtle()
+        enemy2.hideturtle()
