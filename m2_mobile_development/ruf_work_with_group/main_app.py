f3af1075a4d9b123caea263b2fc831bda3723a32
Dmitriy <dmitriy250300@mail.ru>
2025-04-06 14:12:30 +0300

    06.04.2025

--- m2_mobile_development/ruf_work_with_group/main_app.py
+++ m2_mobile_development/ruf_work_with_group/main_app.py
@@ -75,6 +75,7 @@ class PulseScr(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # виджеты экрана:
+        self.next_screen = False
         instr = Label(text=txt_test1)
         # таймер, привязка к логическому свойству
         self.lbl_sec = Seconds(15)
@@ -84,7 +85,7 @@ class PulseScr(Screen):
         self.in_result = TextInput(text='0', multiline=False)
         # выключим окно для ввода текста пока не прошел таймер
         self.in_result.set_disabled(True)
-        self.btn = Button(text='Продолжить:', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn = Button(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
         self.btn.on_press = self.next
         # размещаем виджеты на линиях (лэйаутах)
         line = BoxLayout(size_hint=(0.8, None), height='30sp')
@@ -93,23 +94,37 @@ class PulseScr(Screen):
         # главный вертикальный лэйаут
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         outer.add_widget(instr)
+        outer.add_widget(self.lbl_sec)
         outer.add_widget(line)
         outer.add_widget(self.btn)
         # добавляем главный лэйаут на экран
         self.add_widget(outer)
 
+    def sec_finished(self, *args):
+        self.next_screen = True
+        self.in_result.set_disabled(False)
+        self.btn.set_disabled(False)
+        self.btn.text = 'Продолжить'
+
+
     def next(self):
         """описывает нажатие на кнопку"""
-        global p1
-        # получаем переменную p1
-        p1 = check_int(self.in_result.text)
-        if not p1 or p1 <= 0:
-            # проверяем, ввели ли мы пульс верно - это должно быть число больше 0
-            p1 = 0
-            self.in_result.text = str(p1)
+        if not self.next_screen:
+            # блокируем кнопку пока не прошёл таймер
+            self.btn.set_disabled(True)
+            self.lbl_sec.start()
         else:
-            # если ввели верно - переход на след. экран
-            self.manager.current = 'sits'
+            # таймер окончил работу - получаем значения пульса и идём на след. экран
+            global p1
+            # получаем переменную p1
+            p1 = check_int(self.in_result.text)
+            if not p1 or p1 <= 0:
+                # проверяем, ввели ли мы пульс верно - это должно быть число больше 0
+                p1 = 0
+                self.in_result.text = str(p1)
+            else:
+                # если ввели верно - переход на след. экран
+                self.manager.current = 'sits'
 
 
 class CheckSits(Screen):
@@ -137,15 +152,22 @@ class PulseScr2(Screen):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.stage = 0  # СТАДИЯ ТАЙМЕРА
+        self.next_screen = False  # флаг переключения экрана
         # виджеты экрана:
         instr = Label(text=txt_test3)
         lbl_result1 = Label(text='Результат:', halign='right')
         lbl_result2 = Label(text='Результат после отдыха:', halign='right')
-
+        self.lbl_sec = Seconds(15)  # таймер
+        self.lbl_sec.bind(done=self.sec_finished)  # связываем с логическим свойством
+        self.lbl1 = Label(text='Считайте пульс')
         self.in_result1 = TextInput(text='0', multiline=False)
         self.in_result2 = TextInput(text='0', multiline=False)
+        # блокируем окна ввода
+        self.in_result1.set_disabled(True)
+        self.in_result2.set_disabled(True)
 
-        self.btn = Button(text='Продолжить:', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn = Button(text='Начать:', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
         self.btn.on_press = self.next
         # размещаем виджеты на линиях (лэйаутах)
         line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
@@ -158,28 +180,57 @@ class PulseScr2(Screen):
         # главный вертикальный лэйаут
         outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
         outer.add_widget(instr)
+        outer.add_widget(self.lbl1)
+        outer.add_widget(self.lbl_sec)
         outer.add_widget(line1)
         outer.add_widget(line2)
         outer.add_widget(self.btn)
         # добавляем главный лэйаут на экран
         self.add_widget(outer)
 
+    def sec_finished(self, *args):
+        if self.lbl_sec.done:
+            # закончился очередной отсчёт таймера - проверяем какая сейчас стадия
+            if self.stage == 0:
+                # закончили первый подсчёт, отдыхаем
+                self.stage = 1
+                self.lbl1.text = 'Отдыхайте'
+                # перезапускаем таймер, открываем первое окно для ввода
+                self.lbl_sec.restart(30)
+                self.in_result1.set_disabled(False)
+            elif self.stage == 1:
+                # закончили отдых в 30 сек, считаем пульс снова
+                self.stage = 2
+                self.lbl1.text = 'Считайте пульс'
+                self.lbl_sec.restart(15)  # запускаем повторный подсчёт пульса
+            elif self.stage == 2:
+                # финал подсчёта, переключаем на след. экран
+                self.in_result2.set_disabled(False)
+                self.btn.set_disabled(False) # разблокируем второе окно ввода и кнопку
+                self.btn.text = 'Завершить'
+                self.next_screen = True
+
     def next(self):
         """описывает нажатие на кнопку"""
-        global p2, p3
-        # получаем переменные p2, p3, проверяем чтобы они были числом больше 0
-        # и переключаемся на следующий экран
-        p2 = check_int(self.in_result1.text)
-        p3 = check_int(self.in_result2.text)
-        if not p2:
-            p2 = 0
-            self.in_result1.text = str(p2)
-        elif not p3:
-            p3 = 0
-            self.in_result2.text = str(p3)
+        if not self.next_screen:
+            # блокируем кнопку пока не прошёл таймер
+            self.btn.set_disabled(True)
+            self.lbl_sec.start()
         else:
-            # если ввели верно - переход на след. экран
-            self.manager.current = 'result'
+            global p2, p3
+            # получаем переменные p2, p3, проверяем чтобы они были числом больше 0
+            # и переключаемся на следующий экран
+            p2 = check_int(self.in_result1.text)
+            p3 = check_int(self.in_result2.text)
+            if not p2:
+                p2 = 0
+                self.in_result1.text = str(p2)
+            elif not p3:
+                p3 = 0
+                self.in_result2.text = str(p3)
+            else:
+                # если ввели верно - переход на след. экран
+                self.manager.current = 'result'
 
 
 class Result(Screen):
